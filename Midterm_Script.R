27e40f1413ecd088576fd41d854944306d33e4a3
Ahmed Mokhtar <ahmed.mokhtar@mail.utoronto.ca>
2024-08-02 17:43:55 -0400

    Loaded datasets and necessary packages.

--- Midterm_Script.R
+++ Midterm_Script.R
@@ -1,2 +1,12 @@
 #' Midterm Project - Ahmed Mokhtar
 
+station <- read.csv("station.csv")
+trip <- read.csv("trip.csv")
+weather <- read.csv("weather.csv")
+
+#' Loading necessary packages
+
+library(funModeling)
+library(tidyverse)
+library(Hmisc)
+

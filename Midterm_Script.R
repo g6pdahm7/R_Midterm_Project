783f814c681e53d031b860beb53ae4c6fe2a1227
Ahmed Mokhtar <ahmed.mokhtar@mail.utoronto.ca>
2024-08-05 14:18:19 -0400

    Added the remaining EDAs. Adjusted some notation.

--- Midterm_Script.R
+++ Midterm_Script.R
@@ -27,7 +27,7 @@ trip$zip_code[trip$zip_code == ""] <- NA
 
 #' EDA for trip
 
-basic_eda <- function(trip) {
+basic_eda_t <- function(trip) {
   glimpse(trip)
   print(status(trip))
   freq(trip) 
@@ -36,5 +36,41 @@ basic_eda <- function(trip) {
   describe(trip)
 }
 
-basic_eda(trip)
+basic_eda_t(trip)
 
+#' Checking and replacing empty cells with NAs for station
+sum(station == "", na.rm = T)
+
+#' EDA for station
+
+basic_eda_s <- function(station) {
+  glimpse(station)
+  print(status(station))
+  freq(station) 
+  print(profiling_num(station))
+  plot_num(station)
+  describe(station)
+}
+
+basic_eda_s(station)
+
+#' Checking and replacing empty cells for weather. It was observed that
+#' in the events column, there was no entries when there are no events.
+#' For consistency, I just replaced those with "None" instead of leaving
+#' them blank.
+sum(weather == "", na.rm = T)
+sum(weather$events == "", na.rm = T)
+weather$events[weather$events == ""] <- "None"
+
+#' EDA for weather
+
+basic_eda_w <- function(weather) {
+  glimpse(weather)
+  print(status(weather))
+  freq(weather) 
+  print(profiling_num(weather))
+  plot_num(weather)
+  describe(weather)
+}
+
+basic_eda_w(weather)

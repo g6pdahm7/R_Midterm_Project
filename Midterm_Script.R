59e9a1511932c097c81a0bc785b44522f5b1370a
Ahmed Mokhtar <ahmed.mokhtar@mail.utoronto.ca>
2024-08-05 13:49:46 -0400

    Added simple structural fixes to the data before EDA.

--- Midterm_Script.R
+++ Midterm_Script.R
@@ -10,3 +10,31 @@ library(funModeling)
 library(tidyverse)
 library(Hmisc)
 
+#' Checking and replacing empty cells with NAs for trip
+sum(trip$id == "")
+sum(trip$duration == "")
+sum(trip$start_date == "")
+sum(trip$start_station_name == "")
+sum(trip$start_station_id == "")
+sum(trip$end_date == "")
+sum(trip$end_station_name == "")
+sum(trip$end_station_id == "")
+sum(trip$bike_id == "")
+sum(trip$subscription_type == "")
+sum(trip$zip_code == "")
+trip$zip_code[trip$zip_code == ""] <- NA
+
+
+#' EDA for trip
+
+basic_eda <- function(trip) {
+  glimpse(trip)
+  print(status(trip))
+  freq(trip) 
+  print(profiling_num(trip))
+  plot_num(trip)
+  describe(trip)
+}
+
+basic_eda(trip)
+
